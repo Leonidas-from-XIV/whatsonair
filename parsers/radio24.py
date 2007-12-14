b3e739bb976670ce37228f73d18f13ede6a80d16
Marek Kubica <marek@xivilization.net>
2007-12-14 18:53:30 +0000

    Fixed Radio 24 parser

--- parsers/radio24.py
+++ parsers/radio24.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- encoding: latin-1 -*-
+# -*- encoding: UTF-8 -*-
 
 import base, urllib
 
@@ -7,33 +7,30 @@ class Radio24Parser(base.StationBase):
     """Parser for Radio24"""
     
     __station__ = 'Radio24'
-    __version__ = '0.1.1'
+    __version__ = '0.2.0'
     
-    def __init__(self, url='http://www.radio24.ch/php/now_playing.php',
+    def __init__(self, url='http://www.radio24.ch/ext/webradio/onair_small.php',
         stream='http://dms-cl-011.skypro-media.net/radio24-hi'):
         base.StationBase.__init__(self, url)
     
     def parse(self):
         """Cuts the content and extracts informations"""
-        
-        # split on the &
-        infos = self.pagecontent.split('&')
-        
-        # feed the variables artist and title with the values
-        for info in infos:
-            if info.startswith('interpret='):
-                artist = info.replace('interpret=', '')
-            elif info.startswith('song='):
-                title = info.replace('song=', '')
-        
-        # replace the + by a space
-        self.artist = artist.replace('+', ' ')
-        # as previous, but additionally unquote the HTTP-styled quotes
-        self.title = urllib.unquote(title.replace('+', ' '))
+        # get the soup
+        soup = base.Soup(self.pagecontent)
+        # now the hacking begins (some trouble with BeautifulSoup)
+        # select an element
+        point = base.select(soup, 'tr td i')[0]
+        # get its parent
+        parent = point.parent
+        # get all texts
+        texts = parent.findAll(text=True)
+        # convert and save these
+        self.artist = self.capstext(texts[1])
+        self.title = self.capstext(texts[-1])
     
     def current_track(self):
         """Returns the current playing artist"""
-        return "%s - %s" % (self.capstext(self.artist), self.capstext(self.title))
+        return "%s - %s" % (self.artist, self.title)
 
 Parser = Radio24Parser
 
