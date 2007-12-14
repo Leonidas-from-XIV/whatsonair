652cca065879ad0037813203c51c3b9371e8cb9c
Marek Kubica <marek@xivilization.net>
2007-12-14 20:47:37 +0000

    Fixed Energy Saxonia parser, it now displays the title properly

--- parsers/energysax.py
+++ parsers/energysax.py
@@ -7,19 +7,20 @@ class EnergySaxParser(base.StationBase):
     """Energy in Saxonia"""
     
     __station__ = 'EnergySaxonia'
-    __version__ = '0.3.1'
+    __version__ = '0.3.2'
     
     def __init__(self, url='http://www.energy.de/static/ticker/ticker.php?sender=sachsen'):
         base.StationBase.__init__(self, url)
     
     def parse(self):
         """Call feed first"""
-        self.title = self.cut_content('&song_sac=', '&&artist_sac=')[0]
-        
-        self.artist = self.cut_content('&&artist_sac=', '&')[0]
+        track = self.cut_content_simple('&song_sac=JETZT: ', '&&artist_sac=')[0]
+        artist, title = track.split(' - ', 1)
+        self.artist = self.capstext(artist)
+        self.title = self.capstext(title)
     
     def current_track(self):
-        return "%s - %s" % (self.capstext(self.artist), self.capstext(self.title))
+        return "%s - %s" % (self.artist, self.title)
 
 Parser = EnergySaxParser
 
